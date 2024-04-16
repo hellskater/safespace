7fc9b234f109c68332e998008f361cf8ba9c9da8
K Srinivas Rao <ksrinivasrao531@gmail.com>
2024-04-16 12:29:18 +0530

    additional safe conditions

--- apps/extension/src/background/index.ts
+++ apps/extension/src/background/index.ts
@@ -1,3 +1,3 @@
 import { logHeaders } from "@/lib/background/security-headers"
 
-// logHeaders()
+logHeaders()
--- apps/extension/src/lib/background/security-headers.ts
+++ apps/extension/src/lib/background/security-headers.ts
@@ -1,11 +1,18 @@
+function isRedirect(status: number) {
+  return status >= 300 && status < 400
+}
+
 export const logHeaders = () => {
   chrome.webRequest.onHeadersReceived.addListener(
     function (details) {
+      if (!details.responseHeaders || isRedirect(details.statusCode)) {
+        return
+      }
       if (details.type === "main_frame") {
         console.log("Received headers:", details)
       }
     },
-    { urls: ["<all_urls>"] },
-    ["responseHeaders"]
+    { urls: ["<all_urls>"], types: ["main_frame"] },
+    ["responseHeaders", "extraHeaders"]
   )
 }
