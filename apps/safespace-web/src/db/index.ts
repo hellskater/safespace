7a1eeee53121c25330f09c0ead9460ec3d3a13a1
K Srinivas Rao <ksrinivasrao531@gmail.com>
2024-04-26 07:13:04 +0530

    vercel api error

--- apps/safespace-web/src/db/index.ts
+++ apps/safespace-web/src/db/index.ts
@@ -1,9 +1,9 @@
 import { env } from "@/utils/env";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
-const sql = neon(env.NEON_DATABASE_URL!);
+const pool = new Pool({ connectionString: env.NEON_DATABASE_URL! });
 
-const db = drizzle(sql);
+const db = drizzle(pool);
 
 export default db;
